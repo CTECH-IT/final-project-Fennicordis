a682e01f9ec4209bd0d8ecbcb8dd6e730ff88eea
Alex Hansen <7299702@isd535.org>
2022-06-01 18:23:42 -0500

    Added second ball plus shrinking

--- scripts/main.js
+++ scripts/main.js
@@ -3,14 +3,18 @@ let ctx = canvas.getContext("2d");
 
 let x = canvas.width / 2;
 let y = canvas.height - 30;
+let a = canvas.width / 2;
+let b = canvas.height - 30;
 
 let dx = 3;
 let dy = -3;
+let da = 3;
+let db = -3;
 
 let ballRadius = 10;
 
-let paddleHeight = 7.5;
-let paddleWidth = 85;
+let paddleHeight = 8;
+let paddleWidth = 90;
 let paddleX = (canvas.width - paddleWidth) / 2;
 
 let rightPressed = false;
@@ -18,10 +22,18 @@ let leftPressed = false;
 
 let score = 0;
 
-function drawBall() {
+function drawBall1() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#7dc4ff";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawBall2() {
+    ctx.beginPath();
+    ctx.arc(a, b, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = "#f08991";
     ctx.fill();
     ctx.closePath();
 }
@@ -39,7 +51,33 @@ function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     // draw the ball
-    drawBall();
+    drawBall1();
+
+    // draw the second ball
+    drawBall2();
+
+    // changes x and y values
+    a += da;
+    b += db;
+
+    // check to see if you have hit the edge
+    if (a + da > canvas.width - ballRadius || a + da < ballRadius) {
+        da = -da;
+    }
+    if (b + db < ballRadius) {  // ceiling check
+        db = -db;
+    } else if (b + db > canvas.height - ballRadius) {  // floor check
+        if (a > paddleX && a < paddleX + paddleWidth) {  // paddle check
+            db = -db;
+            score += 1;
+            ballRadius -= 1.2;
+        }
+        else {  // it hit the floor!
+            alert("Game Over! " + "Score: " + score);
+            document.location.reload();
+            clearInterval(interval);  // needed for the browser to end the game
+        }
+    }
 
     // changes x and y values
     x += dx;
